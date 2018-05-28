c5ad1f5fa8884ffe10188877ef8de38eb2a1624b
Marius Bodnariuc <marriusan@yahoo.com>
2018-05-28 09:25:43 +0300

    Add an exhaustive search component for generating work shifts

---
+++ SupportWheelOfFate.Core.Test/ExhaustiveSearchGeneratorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SupportWheelOfFate.Core.Test
+{
+    public class ExhaustiveSearchGeneratorTests
+    {
+        [Fact]
+        public void GeneratesTwoUniqueCombinationsForTwoOptionsAndOnePosition()
+        {
+            var combinations = GenerateAllCombinations(numberOfOptions: 2, numberOfPositions: 1);
+
+            Assert.Equal(2, combinations.Count());
+            Assert.Collection(combinations,
+                firstCombination => Assert.Equal(new List<int> { 0 }, firstCombination),
+                secondCombination => Assert.Equal(new List<int> { 1 }, secondCombination));
+        }
+
+        [Fact]
+        public void GeneratesThirtyTwoUniqueCombinationsForTwoOptionsAndFivePositions()
+        {
+            var combinations = GenerateAllCombinations(numberOfOptions: 2, numberOfPositions: 5);
+
+            Assert.Equal(32, combinations.Count());
+            Assert.Equal(combinations.ToHashSet().ToList(), combinations.ToList());
+            new List<IEnumerable<int>>
+            {
+                new List<int> { 0, 0, 0, 0, 0 },
+                new List<int> { 0, 1, 0, 1, 0 },
+                new List<int> { 1, 1, 0, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1 },
+            }.ForEach(combination => Assert.Contains(combination, combinations));
+        }
+
+        [Fact]
+        public void GeneratesEightyOneUniqueCombinationsForThreeOptionsAndFourPositions()
+        {
+            var combinations = GenerateAllCombinations(numberOfOptions: 3, numberOfPositions: 4);
+
+            Assert.Equal(81, combinations.Count());
+            Assert.Equal(combinations.ToHashSet().ToList(), combinations.ToList());
+        }
+
+        [Fact]
+        public void GeneratesZeroCombinationsForLessThanOneOption()
+        {
+            new List<int> { 0, -3 }.ForEach(options =>
+            {
+                var combinations = GenerateAllCombinations(numberOfOptions: options, numberOfPositions: 3);
+                Assert.Empty(combinations);
+            });
+        }
+
+        [Fact]
+        public void GeneratesOneEmptyCombinationForLessThanOnePosition()
+        {
+            new List<int> { 0, -3 }.ForEach(positions =>
+            {
+                var combinations = GenerateAllCombinations(numberOfOptions: 2, numberOfPositions: positions);
+
+                Assert.Single(combinations);
+                Assert.Collection(combinations,
+                    combination => Assert.Empty(combination));
+            });
+        }
+
+        private IEnumerable<IEnumerable<int>> GenerateAllCombinations(int numberOfOptions, int numberOfPositions)
+        {
+            var generator = new ExhaustiveSearchGenerator(numberOfOptions);
+            return generator.GenerateLazy(numberOfPositions);
+        }
+    }
+}
---
+++ SupportWheelOfFate.Core/ExhaustiveSearchGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SupportWheelOfFate.Core
+{
+    public class ExhaustiveSearchGenerator
+    {
+        private int _numberOfOptions;
+
+        public ExhaustiveSearchGenerator(int numberOfOptions)
+        {
+            _numberOfOptions = numberOfOptions;
+        }
+
+        public IEnumerable<IEnumerable<int>> GenerateLazy(int numberOfPositions)
+        {
+            if (numberOfPositions <= 0)
+            {
+                yield return Enumerable.Empty<int>();
+            }
+            else
+            {
+                for (var option = 0; option < _numberOfOptions; option++)
+                {
+                    foreach (var combo in GenerateLazy(numberOfPositions - 1))
+                    {
+                        yield return new[] { option }.Concat(combo);
+                    }
+                }
+            }
+        }
+    }
+}
